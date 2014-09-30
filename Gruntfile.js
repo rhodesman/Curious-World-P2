d2bd6b7899eb50460d2d0f56813ee1be3ae6fc6c
Jason Rhodes <jason.rhodes@wbstudios.co>
2014-09-30 12:14:25 -0400

    added grunt settings for heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,4 +56,5 @@ module.exports = function (grunt) {
   grunt.registerTask('heroku', ['sass', 'autoprefixer']);
 	grunt.registerTask('build', ['sass', 'autoprefixer']);
 	grunt.registerTask('default', ['build', 'watch']);
+  grunt.registerTask('heroku:development', 'clean less mincss');
 };
