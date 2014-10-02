57e58dc4ecdf025bcdbcbb6e47dd03c8ff64a28a
Jason Rhodes <jason.rhodes@wbstudios.co>
2014-10-02 10:16:13 -0400

    put "/" on wrong side

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + 'landing/'));
+app.use(express.static(__dirname + '/landing'));
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
