64dafbc7ab9141b35834a6cfa3070e96d36fbca3
Andrey Kondratyuk <andrey@dsrp.tv>
2014-10-03 12:00:07 -0400

    *scroll opacity/animation effects

--- home/app.js
+++ home/app.js
@@ -28,6 +28,7 @@
             function (parallaxHelper, $scope, $rootScope, $routeParams, AuthCheck, slick) {
               ng.extend($scope, new AuthCheck($scope)); // Inject authentication checking
 
+              //parallax background-position scroll custom setup
               $scope.positionBackground = function(elementPosition) {
                 var factor = -0.3;
                 var pos = elementPosition.elemY*factor;
@@ -35,8 +36,23 @@
                    backgroundPosition: '0px ' + pos + 'px'
                 };
               };
-
-
             }
-        ]);
+        ])
+        .directive("scrollFade", function($window) {
+          return function(scope, element, attrs) {
+            angular.element($window).bind("scroll", function() {
+              var bottom_of_object = $(element).offset().top + $(element).outerHeight();
+              var bottom_of_window = $(window).scrollTop() + $(window).height();
+              if (bottom_of_object < bottom_of_window) {
+                //in view
+                // element.css('opacity', 1);
+                element.css('opacity', 1);
+              } else {
+                //outside of view
+                element.css('opacity', 0);
+              }
+              scope.$apply();
+            });
+          };
+        });
 }(window.angular));
