735f3b110954188357bfecd3e1d21f4a69f5e26d
Andrey Kondratyuk <andrey@dsrp.tv>
2014-10-06 13:04:43 -0400

    * this push is for testing (please ignore) * losslessly optimized images * improving parallax performance

--- home/app.js
+++ home/app.js
@@ -31,7 +31,7 @@
               // parallax background-position scroll custom setup
               $scope.positionBackground = function(elementPosition) {
                 var factor = -0.4;
-                var pos = (elementPosition.elemY*factor).toFixed();
+                var pos = (elementPosition.elemY*factor);
                 return {
                    backgroundPosition: '0px ' + pos + 'px'
                 };
@@ -45,36 +45,35 @@
           var offset = 100;
 
 
-          function scrollAndFade(){
+          // function scrollAndFade(){
+          //   var top_of_element = element.offset().top + offset;
+          //   var bottom_of_window = ngWindow.scrollTop() + $window.innerHeight;
+          //
+          //   if (top_of_element < bottom_of_window) {
+          //     element[0].style.opacity = 1;
+          //   } else {
+          //     element[0].style.opacity = 0;
+          //   }
+          //   scope.$apply();
+          // }
+          //
+          // var betterScroll = _.debounce(scrollAndFade, 50);
+          //
+          // ngWindow.bind("scroll", function(){
+          //   window.requestAnimationFrame(betterScroll);
+          // });
 
-            var top_of_element = element.offset().top + offset;
-            var bottom_of_window = ngWindow.scrollTop() + $window.innerHeight;
-
-            if (top_of_element < bottom_of_window) {
-              element[0].style.opacity = 1;
-            } else {
-              element[0].style.opacity = 0;
-            }
-            scope.$apply();
-          }
-
-          var betterScroll = _.debounce(scrollAndFade, 50);
-
-          ngWindow.bind("scroll", function(){
-            window.requestAnimationFrame(betterScroll);
-          });
-
-          };
-        })
-        .directive("magicFooter", function($window) {
-          return function(scope, element, attrs) {
-            angular.element($window).bind("scroll", function() {
-              var scroll = $(window).scrollTop();
-              if (scroll > 50) {
-                element.css('bottom', 0);
-              }
-              scope.$apply();
-            });
           };
         });
+        // .directive("magicFooter", function($window) {
+        //   return function(scope, element, attrs) {
+        //     angular.element($window).bind("scroll", function() {
+        //       var scroll = $(window).scrollTop();
+        //       if (scroll > 50) {
+        //         element.css('bottom', 0);
+        //       }
+        //       scope.$apply();
+        //     });
+        //   };
+        // });
 }(window.angular));
