adb4b3962f5b4a258915cea450dc5881ba54d788
Andrey Kondratyuk <andrey@dsrp.tv>
2014-10-06 20:27:17 -0400

    * moved scroll-fade directive out of controller so that it can be used anywhere * attached scroll-fade directive to learning-areas

--- home/app.js
+++ home/app.js
@@ -38,31 +38,6 @@
               };
             }
         ])
-        .directive("scrollFade", function($window) {
-          return function(scope, element, attrs) {
-
-          var ngWindow = angular.element($window);
-          var offset = 100;
-
-          function scrollAndFade(){
-            var top_of_element = element.offset().top + offset;
-            var bottom_of_window = ngWindow.scrollTop() + $window.innerHeight;
-
-            if (top_of_element < bottom_of_window) {
-              element[0].style.opacity = 1;
-            } else {
-              element[0].style.opacity = 0;
-            }
-          }
-
-          var betterScroll = _.debounce(scrollAndFade, 50);
-
-          ngWindow.bind("scroll", function(){
-            window.requestAnimationFrame(betterScroll);
-          });
-
-          };
-        })
         .directive("magicFooter", function($window) {
           return function(scope, element, attrs) {
             var ngWindow = angular.element($window);
