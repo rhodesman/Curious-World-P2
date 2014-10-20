e89f84c31c90d8b2632f58dc2c6fd446f9ffb45d
Andrey Kondratyuk <andrey@dsrp.tv>
2014-10-19 21:00:06 -0400

    * updated border and background styles for milestones

--- flashcards/app.js
+++ flashcards/app.js
@@ -110,7 +110,7 @@
                   skill: "Specific card skill copy",
                   description: "Description Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
                   domain: domains[0],
-                  image: 'http://placehold.it/200x130',
+                  image: '',
                   isComplete: false
                 }, {
                   title: "Title of a Card",
@@ -124,7 +124,7 @@
                   skill: "Specific card skill copy",
                   description: "Description Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
                   domain: domains[2],
-                  image: 'http://placehold.it/200x130',
+                  image: '',
                   isComplete: false
                 }, {
                   title: "Title of a Card",
@@ -138,21 +138,21 @@
                   skill: "Specific card skill copy",
                   description: "Description Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
                   domain: domains[4],
-                  image: 'http://placehold.it/200x130',
+                  image: '',
                   isComplete: false
                 }, {
                   title: "Title of a Card",
                   skill: "Specific card skill copy",
                   description: "Description Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
                   domain: domains[5],
-                  image: 'http://placehold.it/200x130',
+                  image: '',
                   isComplete: false
                 }, {
                   title: "Title of a Card",
                   skill: "Specific card skill copy",
                   description: "Description Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
                   domain: domains[6],
-                  image: 'http://placehold.it/200x130',
+                  image: '',
                   isComplete: false
                 }, {
                   title: "Title of a Card",
