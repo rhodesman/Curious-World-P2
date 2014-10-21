e7e2980611bc8a01704092210d1390e907e67119
Andrey Kondratyuk <andrey@dsrp.tv>
2014-10-21 14:45:07 -0400

    *setup position change on flashcard milestones

---
+++ main/directives/position-scroll.js
@@ -0,0 +1,42 @@
+(function (ng) {
+    "use strict";
+
+    ng.module("app")
+        .directive("positionScroll", function($window, $timeout) {
+          return function(scope, element, attrs) {
+
+          $timeout(function(){
+
+            var ngWindow = angular.element($window);
+
+            function scrollAndPosition(){
+              var offset = 50;
+              var cards = document.getElementById("cards");
+              var cardHeight = 515;//update this to element height
+              var bottom_of_cards = cards.offsetTop + cardHeight + offset;
+              var top_of_element = element.offset().top;
+
+              // console.log('bottom of cards:' + bottom_of_cards);
+              // console.log('top of element:' + top_of_element);
+
+              if (top_of_element > bottom_of_cards) {
+                element[0].style.position = 'relative';
+                element[0].style.bottom = 0;
+              } else {
+                element[0].style.position = 'fixed';
+              }
+            }
+
+            scrollAndPosition();
+
+            ngWindow.bind("scroll", function(){
+              scrollAndPosition();
+            });
+
+          });
+
+
+
+          };
+        });
+}(window.angular));
