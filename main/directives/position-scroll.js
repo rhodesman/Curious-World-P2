ae1d7c2fee3a5fa10c21c7d1ae15d28449994748
Andrey Kondratyuk <andrey@dsrp.tv>
2014-10-21 17:47:07 -0400

    * adjustments to flashcard/milestone position

--- main/directives/position-scroll.js
+++ main/directives/position-scroll.js
@@ -1,4 +1,4 @@
-(function (ng) {
+(function (ng, $) {
     "use strict";
 
     ng.module("app")
@@ -10,17 +10,18 @@
             var ngWindow = angular.element($window);
 
             function scrollAndPosition(){
-              var offset = 240;
-              var cards = document.getElementById("cards");
+              var offset = 72;
+              var cards = document.getElementById('cards');
               var cardHeight = 515;//update this to element height
               var bottom_of_cards = cards.offsetTop + cardHeight + offset;
-              var top_of_element = ngWindow.scrollTop() + element.offset().top;
+              var top_of_element = element.offset().top;
 
               console.log('bottom of cards:' + bottom_of_cards);
               console.log('top of element:' + top_of_element);
 
               if (top_of_element >= bottom_of_cards) {
                 element[0].style.position = 'static';
+                element[0].style.bottom = 0;
               } else {
                 element[0].style.position = null;
                 element[0].style.bottom = null;
@@ -39,4 +40,4 @@
 
           };
         });
-}(window.angular));
+}(window.angular, window.jQuery));
